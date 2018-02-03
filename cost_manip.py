195d41579dee68791c839464466dbad1c07fa31e
Yo Sato <yosato16@gmail.com>
2018-02-03 13:14:42 +0000

    refactor cost manip

--- cost_manip.py
+++ cost_manip.py
@@ -1,26 +1,42 @@
 import time
+
 def main0(BaselineFP,OtherFPs):
     BLSentAv,BLWdAv=average_sent_word_cost(BaselineFP)
     print(BLSentAv);print(BLWdAv)
     for FP in OtherFPs:
         Sents=pick_positive_distances(FP,BLSentAv,BLWdAv)
+        Sents1=[];Sents2=[];Sents3=[];Sents4=[]
         for Sent in Sents:
-            print(Sent)
+            if Sent[-1]<50000:
+                Sents1.append(Sent)
+            elif Sent[-1]<70000:
+                Sents2.append(Sent)
+            elif Sent[-1]<90000:
+                Sents3.append(Sent)
+            else:
+                Sents4.append(Sent)
+        Sents=(Sents1,Sents2,Sents3,Sents4)
+        for Cntr,SentSet in enumerate(Sents):
+            print('Category '+str(Cntr+1))
             time.sleep(3)
+            for Sent in SentSet:
+                print(Sent)
+                time.sleep(1)
 
 def pick_positive_distances(FP,BLSentAv,BLWdAv):
     Sents=[]
+    Buffer=30000
     for SentCntr,SentStat in enumerate(generate_sentstat(FP)):
         if not SentStat:
             break
         SentCost=SentStat[-1][-1]
-        SentCostDiff=SentCost-BLSentAv
-        if SentCostDiff>0:
+        SentCostDiff=round(SentCost-BLSentAv,2)
+        if SentCostDiff>Buffer:
             WdStrs=[];WdCosts=[]
             for Cntr,WdTuple in enumerate(SentStat):
                 WdStrs.append(WdTuple[0])
-                WdCostDiff=WdTuple[-1]-BLWdAv
-                if WdCostDiff>0:
+                WdCostDiff=round(WdTuple[-1]-BLWdAv,2)
+                if WdCostDiff>Buffer:
                     WdCosts.append((Cntr,WdCostDiff))
             Sents.append((SentCntr+1,WdStrs,SentCost,WdCosts,SentCostDiff))
         
